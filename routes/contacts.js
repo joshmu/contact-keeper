ebab56039e94939a9b3aff6dcb08062cc0098721
Josh Mu <mu@joshmu.com>
2020-02-14 15:20:32 +1100

    created api routes

---
+++ routes/contacts.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const router = express.Router()
+
+// @route   GET /api/contacts
+// @desc    Get all contacts
+// @access  Private
+router.get('/', (req, res) => {
+  res.send('Get all contacts')
+})
+
+// @route   POST /api/contacts
+// @desc    Add new contact
+// @access  Private
+router.post('/', (req, res) => {
+  res.send('Add new contact')
+})
+
+// @route   PUT /api/contacts/:id
+// @desc    Update contact
+// @access  Private
+router.put('/:id', (req, res) => {
+  res.send('Update contact')
+})
+
+// @route   DELETE /api/contacts/:id
+// @desc    Remove contact
+// @access  Private
+router.delete('/:id', (req, res) => {
+  res.send('Remove contact')
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -6,4 +6,8 @@ app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the Contact Keeper API.' })
 )
 
+app.use('/api/auth', require('./routes/auth.js'))
+app.use('/api/users', require('./routes/users.js'))
+app.use('/api/contacts', require('./routes/contacts.js'))
+
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
