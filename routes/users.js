825d178f01b7e38128572d25c8f501675dffc565
Josh Mu <mu@joshmu.com>
2020-02-15 18:29:39 +1100

    added user model and user route validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
+
+const User = require('../models/User.js')
 
 // @route   POST /api/users
 // @desc    Register users
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Register a user')
-})
+router.post(
+  '/',
+  [
+    check('name', 'Please provide name.')
+      .not()
+      .isEmpty(),
+    check('email', 'Please provide email.').isEmail(),
+    check(
+      'password',
+      'Password needs to be minimum of 6 characters.'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() })
+    }
+    res.json('Passed!')
+  }
+)
 
 module.exports = router
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express()
 // Connect to Database
 connectDB()
 
+// Init Middleware
+app.use(express.json({ extended: false }))
+
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the Contact Keeper API.' })
 )
