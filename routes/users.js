cbb9f97848d2aa248c7a661aba5180d8e5e58736
Josh Mu <mu@joshmu.com>
2020-02-17 11:51:23 +1100

    added user registration to mongodb

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 const { check, validationResult } = require('express-validator')
 
 const User = require('../models/User.js')
@@ -14,17 +15,40 @@ router.post(
       .not()
       .isEmpty(),
     check('email', 'Please provide email.').isEmail(),
-    check(
-      'password',
-      'Password needs to be minimum of 6 characters.'
-    ).isLength({ min: 6 })
+    check('password', 'Password needs to be minimum of 6 characters.')
+      .isString()
+      .isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() })
     }
-    res.json('Passed!')
+
+    const { name, email, password } = req.body
+
+    try {
+      const user = await User.findOne({ email })
+      if (user) {
+        return res.status(500).json({ msg: 'User already exists' })
+      }
+
+      const createdUser = new User({
+        name,
+        email,
+        password
+      })
+
+      const salt = await bcrypt.genSalt(10)
+      createdUser.password = await bcrypt.hash(password, salt)
+
+      await createdUser.save()
+
+      res.json({ msg: 'User saved to database.' })
+    } catch (error) {
+      console.error(error)
+      res.status(500).send('Server Error')
+    }
   }
 )
 
