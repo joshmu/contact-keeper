0a2955a972b1305bb17ba0f14387e72d2682707a
Josh Mu <mu@joshmu.com>
2020-02-18 19:27:17 +1100

    filter contacts

---
+++ client/src/components/contacts/ContactFilter.js
@@ -0,0 +1,37 @@
+import React, { useContext, useRef, useEffect } from 'react'
+import ContactContext from '../../context/Contact/conactContext'
+
+const ContactFilter = () => {
+  const contactContext = useContext(ContactContext)
+  const { filtered, filterContacts, clearFilter } = contactContext
+
+  const text = useRef('')
+
+  useEffect(() => {
+    if (filtered === null) {
+      text.current.value = ''
+    }
+  })
+
+  const onChange = e => {
+    if (text.current.value !== '') {
+      filterContacts(e.target.value)
+    } else {
+      clearFilter()
+    }
+  }
+
+  return (
+    <form onSubmit={e => e.preventDefault()} autoComplete="off">
+      <input
+        ref={text}
+        type="text"
+        name="search"
+        placeholder="Search Contacts..."
+        onChange={onChange}
+      />
+    </form>
+  )
+}
+
+export default ContactFilter
--- client/src/context/Contact/ContactState.js
+++ client/src/context/Contact/ContactState.js
@@ -37,7 +37,8 @@ const ContactState = props => {
         type: 'professional'
       }
     ],
-    current: null
+    current: null,
+    filtered: null
   }
 
   const [state, dispatch] = useReducer(contactReducer, initialState)
@@ -69,14 +70,23 @@ const ContactState = props => {
   }
 
   // Filter Contacts
+  const filterContacts = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text })
+  }
 
   // Clear Filter
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER })
+  }
 
   return (
     <contactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
+        filterContacts,
+        clearFilter,
         setCurrent,
         clearCurrent,
         addContact,
