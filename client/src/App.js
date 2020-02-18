af6c4e5732ac145f118c8ecb41de60c547baf573
Josh Mu <mu@joshmu.com>
2020-02-18 13:17:45 +1100

    added router, home & about pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/layout/Navbar'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
 
 const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Fragment>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Router>
+    </Fragment>
   )
 }
 
