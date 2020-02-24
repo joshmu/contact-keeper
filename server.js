f3259d2bbe8aab7bd33d67c688f1897d1b93ea52
Josh Mu <mu@joshmu.com>
2020-02-24 17:23:37 +1100

    setup for heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db.js')
+const path = require('path')
 
 const PORT = process.env.PORT || 5000
 const app = express()
@@ -10,12 +11,18 @@ connectDB()
 // Init Middleware
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the Contact Keeper API.' })
-)
-
 app.use('/api/auth', require('./routes/auth.js'))
 app.use('/api/users', require('./routes/users.js'))
 app.use('/api/contacts', require('./routes/contacts.js'))
+
+// Serve static assets in production (react) << make sure this is after other routes
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder (react client build folder)
+  app.use(express.static('client/build'))
+  // Serve index.html file when any other route unspecified is hit
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+}
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
